69b1620b98d093897d3e265eab619f7bae9a94d3
Jinwoo <ozi0414@gmail.com>
2018-10-18 12:48:31 -0500

    add import question component

--- client/src/components/ImportQ.js
+++ client/src/components/ImportQ.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 import Nav from './Nav';
 
 export default class ImportQ extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: '',
+      anwers: [],
+    };
+  }
+
   render() {
     return (
       <div>
         <Nav />
         <p>ImportQ Component</p>
-        <button>Import from CSV</button>
+        <button
+          onClick={() => {
+            console.log('Heeoe');
+          }}
+        >
+          Import from CSV
+        </button>
       </div>
     );
   }
