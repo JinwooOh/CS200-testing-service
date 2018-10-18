aeafbea630c05c1fe1b45ecee31ca73cab9dd0ad
Jinwoo <ozi0414@gmail.com>
2018-10-18 13:33:17 -0500

    implment json import from local

--- client/src/components/ImportQ.js
+++ client/src/components/ImportQ.js
@@ -1,27 +1,53 @@
 import React, { Component } from 'react';
+import Files from 'react-files';
 import Nav from './Nav';
 
 export default class ImportQ extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      questions: '',
-      anwers: [],
+      jsonFile: {},
+    };
+    this.fileReader = new FileReader();
+    this.fileReader.onload = event => {
+      this.setState({ jsonFile: JSON.parse(event.target.result) }, () => {
+        console.log(this.state.jsonFile);
+      });
     };
   }
+
+  handleLoadJson = file => {
+    const fileReader = new FileReader();
+    fileReader.onload = event => {
+      this.setState({ jsonFile: JSON.parse(event.target.result) }, () => {
+        console.log(this.state.jsonFile);
+      });
+    };
+    fileReader.readAsText(file[0]);
+  };
 
   render() {
     return (
       <div>
         <Nav />
-        <p>ImportQ Component</p>
-        <button
-          onClick={() => {
-            console.log('Heeoe');
+        <h3>ImportQ Component</h3>
+
+        <p>Import JSON file</p>
+        <Files
+          className="files-dropzone"
+          onChange={file => {
+            this.fileReader.readAsText(file[0]);
           }}
+          onError={err => console.log(err)}
+          accepts={['.json']}
+          multiple
+          maxFiles={3}
+          maxFileSize={10000000}
+          minFileSize={0}
+          clickable
         >
-          Import from CSV
-        </button>
+          Click here to upload JSON file
+        </Files>
       </div>
     );
   }
