9c1f0af1b5404cee0c9f25be38c4cec590227956
Junior Quintero <JuniorQ@tasconline.com>
2018-10-31 06:58:18 -0500

    Created complete dummy data

--- server.js
+++ server.js
@@ -30,9 +30,43 @@ var answerSchema = new Schema ({
   answer:  String
 });
 
+var userSchema = new Schema ({
+  loginType : String,
+  name:        String,
+  userId:          String,
+  updated:     Date,
+  exams:    [mongoose.Schema.Types.ObjectId],
+  studentList: [mongoose.Schema.Types.ObjectId]
+});
+
+var examSchema = new Schema ({
+   timeLimit: Number,
+   courseName: String,
+   courseNumber: Number,
+   dateCreated: Date,
+   updated:     Date,
+   avgScore: Number,
+   medianScore: Number,
+   highestScore  :Number,
+   lowestScore   :Number,
+   createdBy: mongoose.Schema.Types.ObjectId,
+   questions:    [mongoose.Schema.Types.ObjectId]
+});
+
+var scoreSchema = new Schema ({
+   examId:  mongoose.Schema.Types.ObjectId,
+   dateTaken:  Date,
+   userId:    mongoose.Schema.Types.ObjectId,
+   userAnswers:    [mongoose.Schema.Types.ObjectId],
+   relevantQuestions: [mongoose.Schema.Types.ObjectId]
+ });
+
 // Compile model from schema
 var Question = mongoose.model('Question', questionSchema);
 var Answer = mongoose.model('Answer', answerSchema );
+var Exam = mongoose.model('Exam', examSchema );
+var User = mongoose.model('User', userSchema );
+var Score = mongoose.model('Score', scoreSchema );
 
 // // create instances of the models
 
@@ -53,6 +87,36 @@ var question_1 = new Question({
 question_1.answers.push(answer_1._id);
 question_1.answers.push(answer_2._id);
 
+var exam_1 = new Exam({
+   timeLimit: 60,
+   courseName: "Software Engineering",
+   courseNumber: 1234,
+   dateCreated: Date.now(),
+   updated:     Date.now(),
+   avgScore: 9.8,
+   medianScore: 9.75,
+   highestScore  :10,
+   lowestScore   :9.5,
+})
+
+var user_1 = new User({
+  loginType : 'Admin',
+  name:        'Mark',
+  userId:          'mrenault',
+  updated:     Date.now(),
+})
+user_1.exams.push(exam_1._id)
+exam_1.userId = user_1._id
+
+var score_1 = new Score({
+   examId:  exam_1._id,
+   dateTaken:  Date.now(),
+   userId:    user_1._id,
+})
+score_1.relevantQuestions.push(question_1._id)
+score_1.userAnswers.push(answer_1._id)
+
+
  answer_1.save(function (err) {
   if (err) return handleError(err);
    // saved!
@@ -62,7 +126,22 @@ question_1.answers.push(answer_2._id);
    // saved!
  });
  // Save the data
-question_1.save(function (err) {
+ question_1.save(function (err) {
+  if (err) return handleError(err);
+   // saved!
+ });
+
+ exam_1.save(function (err) {
+  if (err) return handleError(err);
+   // saved!
+ });
+
+ user_1.save(function (err) {
+  if (err) return handleError(err);
+   // saved!
+ });
+
+ score_1.save(function (err) {
   if (err) return handleError(err);
    // saved!
  });
