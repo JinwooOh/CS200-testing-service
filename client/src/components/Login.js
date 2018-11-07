6bc0c60ceb629f9fc2374b74df9d2498187e6213
Catherine Zhang <mzhang335@wisc.edu>
2018-11-07 09:02:19 -0600

    Redirection from client page to server login api

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import Nav from './Nav';
 
 export default class Login extends Component {
+  state = {
+    redirect: false
+  }
+  setRedirect = () => {
+    this.setState({
+      redirect: true
+    })
+  }
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      return <Redirect to='${baseUrl}:5000/api/login' />
+    }
+  }
   render() {
     return (
       <div>
         <Nav />
         <p>Welcome</p>
         <ul>
-          <button type="button">Login</button>
+	    {this.renderRedirect()}
+            <button onClick={this.setRedirect}>Login</button>
         </ul>
       </div>
     );
--- server.js
+++ server.js
@@ -2,9 +2,54 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require('./routes/routes');
-
+const passport = require('passport');
 const app = express();
+const saml = require('passport-saml');
+const fs = require('fs');
 //https://mongoosejs.com/docs/populate.html
+
+var samlStrategy = new saml.Strategy({
+  // URL that goes from the Identity Provider -> Service Provider
+  callbackUrl: '/api/login/callback',
+  // URL that goes from the Service Provider -> Identity Provider
+  entryPoint: 'https://auth.miniorange.com/moas/idp/samlsso',
+  // Usually specified as `/shibboleth` from site root
+  issuer: "passport-saml",
+  // Service Provider Certificate
+  privateCert: fs.readFileSync(__dirname + '/cert/private_key.pem', 'utf8'),
+  // Identity Provider's public key
+  cert: fs.readFileSync(__dirname + '/cert/idp_cert.pem', 'utf8')
+}, function(profile, done) {
+  return done(null, profile); 
+});
+
+passport.use(samlStrategy);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.get('/api/login',
+  passport.authenticate('saml', { failureRedirect: '/login/fail' }),
+  function (req, res) {
+    res.redirect('/');
+  }
+);
+
+app.post('/api/login/callback',
+   passport.authenticate('saml', { failureRedirect: '/login/fail' }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
+app.get('/login/fail', 
+  function(req, res) {
+    res.status(401).send('Login failed');
+  }
+);
+
+
 
 //DB setting
 var mongoDB =
