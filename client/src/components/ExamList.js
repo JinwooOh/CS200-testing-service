9907771bea6d3beba51cdfddfd88823005fc3276
Jinwoo <ozi0414@gmail.com>
2018-11-10 17:06:30 -0600

    formatting code

--- client/src/components/ExamList.js
+++ client/src/components/ExamList.js
@@ -2,34 +2,33 @@ import React, { Component } from 'react';
 import Nav from './Nav';
 
 export default class ExamList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          examList: [],
-        };
-      }
-    
-      componentDidMount() {
-        fetch('/api/pullExamList')
-        // .then(res => res.json())
-        .then(res=>res.json())
-        .then(res=>this.setState({examList: res}))
-          .catch(err => {
-            console.log(err, 'failed to fetch');
-          });
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      examList: [],
+    };
+  }
 
+  componentDidMount() {
+    fetch('/api/pullExamList')
+      // .then(res => res.json())
+      .then(res => res.json())
+      .then(res => this.setState({ examList: res }))
+      .catch(err => {
+        console.log(err, 'failed to fetch');
+      });
+  }
 
-    render() {
-        return (
-        <div>
-            <Nav />
-            <p>ExamList Component</p>
-            <ul>
-                <li>Exam 1</li>
-                <li>Exam 2</li>
-            </ul>
-        </div>
+  render() {
+    return (
+      <div>
+        <Nav />
+        <p>ExamList Component</p>
+        <ul>
+          <li>Exam 1</li>
+          <li>Exam 2</li>
+        </ul>
+      </div>
     );
   }
 }
