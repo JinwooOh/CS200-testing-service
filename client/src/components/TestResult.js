df3a83ec04e6ce28bb14b3d962fd78be43b816d2
Jinwoo <ozi0414@gmail.com>
2018-11-11 13:24:24 -0600

    formatting

--- client/src/components/TestResult.js
+++ client/src/components/TestResult.js
@@ -2,35 +2,33 @@ import React, { Component } from 'react';
 import Nav from './Nav';
 
 export default class TestResult extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {};
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch('/api/fetchExams', {
-      method: "GET",
+      method: 'GET',
       headers: {
         'Content-Type': 'application/json',
-      }
+      },
     })
       .then(res => {
-        
-        //console.log(res.json());
+        // console.log(res.json());
         return res.json();
       })
       .then(res => {
-        
         this.setState({ exams: res });
-        
       })
       .catch(error => console.error('fetch error at pull exams', error)); // error
   }
+
   render() {
     console.log(this.state);
-    var exams;
-    if (this.state.exams != undefined){
-      exams = this.state.exams.map((item,i) => <li key={i}>{item.courseName}</li>)
+    let exams;
+    if (this.state.exams != undefined) {
+      exams = this.state.exams.map((item, i) => <li key={i}>{item.courseName}</li>);
     }
     return (
       <div>
