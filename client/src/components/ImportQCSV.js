25f592c710c260c0ec28853827756fb58e79e6ca
Junior Quintero <JuniorQ@tasconline.com>
2018-11-13 17:57:59 -0600

    Uploaded CSV data and post to console

--- client/src/components/ImportQCSV.js
+++ client/src/components/ImportQCSV.js
@@ -1,39 +1,58 @@
 import React, { Component } from 'react';
 import Files from 'react-files';
 
+let fileReader;
 export default class ImportQCSV extends Component {
-  constructor(props) {
-    super(props);
+ constructor(props) {
+   super(props);
 
-    this.fileReader = new FileReader();
-    this.fileReader.onload = event => {
-      this.props.updateJson({ jsonFile: JSON.parse(event.target.result) }, () => {
-        console.log(this.state.jsonFile);
-      });
+   this.fileReader = new FileReader();
+   this.fileReader.onload = event => {
+     this.props.updateJson({ jsonFile: JSON.parse(event.target.result) }, () => {
+       console.log(this.state.jsonFile);
+     });
+   };
+ }
 
-    };
-  }
+ handleFileRead = e => {
+   const content = fileReader.result;
+   console.log(content);
+   // … do something with the ‘content’ …
+ };
 
-  render() {
-    return (
-      <div>
-        <h4>Import CSV</h4>
-        <Files
-          className="files-dropzone"
-          onChange={file => {
-            this.fileReader.readAsText(file[0]);
-          }}
-          onError={err => console.log(err)}
-          accepts={['.json']}
-          multiple
-          maxFiles={3}
-          maxFileSize={10000000}
-          minFileSize={0}
-          clickable
-        >
-          <button>Import CSV</button>
-        </Files>
-      </div>
-    );
-  }
+ handleFileChosen = file => {
+   fileReader = new FileReader();
+   fileReader.onloadend = this.handleFileRead;
+   fileReader.readAsText(file);
+ };
+
+ render() {
+   return (
+     <div>
+       <h4>Import CSV</h4>
+       <input
+         type="file"
+         id="file"
+         className="file"
+         accept=".csv"
+         onChange={e => this.handleFileChosen(e.target.files[0])}
+       />
+       {/* <Files
+         className=“files-dropzone”
+         onChange={file => {
+           this.fileReader.readAsText(file[0]);
+         }}
+         onError={err => console.log(err)}
+         accepts={[‘.json’]}
+         multiple
+         maxFiles={3}
+         maxFileSize={10000000}
+         minFileSize={0}
+         clickable
+       >
+         <button>Import CSV</button>
+       </Files> */}
+     </div>
+   );
+ }
 }
