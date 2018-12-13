07830db9f7546329442e3989a046a10f86af94fd
Nam Pham <npham6@wisc.edu>
2018-12-13 14:00:39 -0600

    Finish unitTest for deleteAnswerToQuestion API

--- test/deleteAnswerToQuestion.test.js
+++ test/deleteAnswerToQuestion.test.js
@@ -1,25 +1,44 @@
 /**
  * Created by Monica Ceisel on 12/2/2018.
  */
-const Answer = require('../models/Answer');
-const Question = require('../models/Question');
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var should = chai.should();
+var app = require('../server');
 const assert = require('assert');
-const request = require('supertest');
-const app = require('../server');
+chai.use(chaiHttp);
+const Answer = require('../models/Answer');
+
+
+// @TODO: pull a list of answer first.
 
 /* Needs to be updated to be dynamic*/
 describe('Test deleteAnswerToQuestion API', function() {
     it('handles a DELETE requests to /api/deleteAnswerToQuestion/:id/', function(done) {
-        chai.request(server)
-            .delete('/api/deleteAnswerToQuestion/5c048d0a2590b90ad069ee6c')
-            .end(function(err, res){
-                res.should.have.status(200);
-                res.should.be.json;
-                res.body.should.be.a('object');
-                done();
-            });
+        chai.request(app)
+            .get('/api/pullAnswer')
+            .end(function(err, res) {
+                console.log('respoind is ' , res.body[0]);
+                var oldLength = res.body.length;
+                console.log('number of old answers in the collection is ', oldLength);
+                chai.request(app)
+                .delete('/api/deleteAnswerToQuestion/' + res.body[0]._id)
+                .end(function(err, res){
+                    // 5bef3d795ee8d70c374d8715
+                    Answer.countDocuments().then((count) => {
+                        console.log('number of old answers in the collection is ', count);
+                        assert(count + 1 === oldLength);
+                    });
+                    res.should.have.status(200);
+                    res.body.should.have.property('answer'); // make sure it's an answer
+                    res.body.should.have.property('_id');
+                    res.body.should.be.a('object');
+                    done();
+                });
+            }); 
     });
 });
+
 
 
 
